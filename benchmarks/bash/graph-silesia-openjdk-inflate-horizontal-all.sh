ed83a94ea4879fb7c0ccd5d31fdbc57f1f9bb6cf
Rui Li <ruiamzn@amazon.com>
2022-12-19 03:48:48 +0000

    Add bash for creating inflate merge diagrams

---
+++ benchmarks/bash/graph-silesia-openjdk-inflate-horizontal-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                                             
+set -xue
+                                                                        
+CPU=${CPU:-"Xeon E5-2686 v4 / 1900MHz"}                                        
+DIR=${INFLATE_DIR:-"Xeon-E52686-v4-1900MHz-silesia-2022-12-14-01-old-code-inflate"}       
+                                                                        
+java \
+-cp build/benchmarks/ io.simonis.CreateVegaLiteGraph \
+-legend "" \
+-title "Inflater throughput for Silesia corpus on $CPU" \
+-template benchmarks/java/io/simonis/file-throughput-horizontal.json \
+-map "openjdk-bundled:ojdk / zlib-bundled,openjdk-system:ojdk / zlib-system,openjdk-cloudflare:ojdk / zlib-cloudflare,openjdk-chromium:ojdk / zlib-chromium" \
+-sort "ojdk / zlib-bundled,ojdk / zlib-system,ojdk / zlib-cloudflare,ojdk / zlib-chromium" \
+-json graphs/$DIR/file-inflate-silesia-horizontal-all.json \
+-svg graphs/$DIR/file-inflate-silesia-horizontal-all.svg \
+-png graphs/$DIR/file-inflate-silesia-horizontal-all.png \
+-default-impl ristretto19-madler results/$DIR/ristretto*.json
+
+
